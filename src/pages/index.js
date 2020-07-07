4bb4bacc892057d8530e91dab73c179f1eceb829
Renato S Moreno <peanuts@work.co>
2020-07-07 12:19:12 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,42 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site. My teste 3.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+
+export const pageQuery = graphql`
+  query {
+    allGoogleSpreadsheetSheet1 {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+
+const IndexPage = ({ data }) => {
+  const items = data.allGoogleSpreadsheetSheet1.edges;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site. My teste 3.</p>
+      <p>Now go build something great.</p>
+
+
+      {items.map(({ node }) => (
+        <p key={node.id}>{node.name}</p>
+      ))}
+
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
